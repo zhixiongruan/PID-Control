cacf4b62429f00affd82f272f994f66eb6112294
ruanzhixiong <ruanzx1130@163.com>
2018-09-13 01:04:12 +0800

    PID实现及录屏

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  double total_error = - Kp*p_error - Ki*i_error - Kd*d_error;
+  if (total_error > 1.0) total_error=1.0;
+  if (total_error < -1.0) total_error = -1.0;
+  return total_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,12 @@ int main()
   PID pid;
   // TODO: Initialize the pid variable.
   // Final Parameters.
-  pid.Init(0.15, 0.0, 2.5);
+
+   //pid.Init(1.0, 0.0, 0.0);
+  //pid.Init(0.0, 0.1, 0.0);
+  //pid.Init(0.0, 0.0, 1.0);
+  
+  pid.Init(0.15, 0.0, 2.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
